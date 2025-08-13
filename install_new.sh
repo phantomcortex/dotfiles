13faceb484ae566b6abc10835863a27e3791eb08
phantom <137958098+phantomcortex@users.noreply.github.com>
2025-08-13 05:25:17 -0600

    omz added

--- install_new.sh
+++ install_new.sh
@@ -19,7 +19,7 @@ P10K_THEME_DIR="$OH_MY_ZSH_DIR/custom/themes/powerlevel10k"
 # 4. zsh-syntax & autosuggestions to custom/plugins
 
 if [[ -d ~/.dotfiles ]]; then
-  echo "dotfiles already exists"
+  echo ".dotfiles already exists..."
   if [[ -d ~/.dotfiles_bak ]]; then
     echo "dotfiles_bak already exists aswell"
     read -p "remove dotfiles_bak? (y/n)" yn
@@ -30,10 +30,26 @@ if [[ -d ~/.dotfiles ]]; then
     esac
   else
     mv ~/.dotfiles ~/.dotfiles_bak
-  else
-    #curl 
+  fi
+else
+    #curl
+    #sh -c "$(curl -fsSL https://raw.githubusercontent.com/phantomcortex/dotfiles/main/install_new.sh)"
+    git clone https://github.com/phantomcortex/dotfiles.git ~/.dotfiles #future proof     
+    if [[ -L "~/.zshrc" ]]; then
+      echo "DEBUG:.zshrc is already symlinked"
+      #Don't need to do anything because .zshrc probably isn't going to be linked to anything else
+    else
+      read -p "1:remove .zshrc or 2:move to .zshrc_bak (1/2)" 12
+    case $yn in 
+      [1]* ) rm -rf ~/.zshrc; echo "~/.zshrc removed.\nsymlink back .zshrc in .dotfiles"; ln -s ~/.dotfiles/.zshrc ~/.zshrc;;
+      [2]* ) mv ~/.zshrc ~/.zshrc_bak;echo "DEBUG: mv ~/.zshrc ~/.zshrc_bak";;
+      * ) echo "need a number...exiting >>>>";exit;;
+    esac
+    fi
     #my script
     #oh-my-zsh
+    ZSH=~/.dotfiles/.oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
     #p10k
     #zsh-syntax
     #autosuggestions
